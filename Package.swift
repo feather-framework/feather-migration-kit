e0115b3debbd1e8a8e086c726eac3a90a41507dd
Tibor Bodecs <mail.tib@gmail.com>
2024-03-21 11:44:17 +0100

    updated identifier

--- Package.swift
+++ Package.swift
@@ -11,12 +11,13 @@ let package = Package(
         .visionOS(.v1),
     ],
     products: [
-        .library(name: "MigrationKit", targets: ["MigrationKit"]),
+        .library(name: "MigrationKit", targets: ["MigrationKit"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log", from: "1.0.0"),
         .package(
-            url: "https://github.com/feather-framework/feather-component", .upToNextMinor(from: "0.4.0")
+            url: "https://github.com/feather-framework/feather-component",
+            .upToNextMinor(from: "0.4.0")
         ),
     ],
     targets: [
@@ -24,7 +25,18 @@ let package = Package(
             name: "MigrationKit",
             dependencies: [
                 .product(name: "Logging", package: "swift-log"),
-                .product(name: "FeatherComponent", package: "feather-component"),
+                .product(
+                    name: "FeatherComponent",
+                    package: "feather-component"
+                ),
+            ]
+        ),
+        .testTarget(
+            name: "MigrationKitTests",
+            dependencies: [
+                .target(
+                    name: "MigrationKit"
+                )
             ]
         ),
     ]
